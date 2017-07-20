eeac4fffbf346cec8823bf7c5e4a1a52fba74dff
Parham Alvani <parham.alvani@gmail.com>
2017-07-20 19:45:47 +0430

    SIML parser basis were added.

---
+++ siml/parser.py
@@ -0,0 +1,12 @@
+# In The Name Of God
+# ========================================
+# [] File Name : parser.py
+#
+# [] Creation Date : 20-07-2017
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+
+class SIMLParser:
+    pass
