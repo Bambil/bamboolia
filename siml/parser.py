359f940de8919a37124ef78105c456b658802337
Parham Alvani <parham.alvani@gmail.com>
2017-07-20 20:30:33 +0430

    SIML parser was added.

--- siml/parser.py
+++ siml/parser.py
@@ -6,7 +6,21 @@
 #
 # [] Created By : Parham Alvani (parham.alvani@gmail.com)
 # =======================================
+import os
+import yaml
+
+from .model import Model
 
 
 class SIMLParser:
-    pass
+    @staticmethod
+    def parse(model_root, model_file):
+        with open(os.path.join(model_root, model_file), 'r') as f:
+            try:
+                model = yaml.load(f)
+                if model['package'] != '.%s' % model_root.replace('/', '.'):
+                    raise Exception()
+                return Model()
+                # models['%s.%s' % (model['package'], model['name'])] = model
+            except Exception:
+                pass
