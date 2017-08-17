51d829e1d2ce2d6c9da5cc11b06c03d24ee5d6e3
Parham Alvani <parham.alvani@gmail.com>
2017-08-18 00:18:35 +0430

    Adds agent model.

---
+++ src/agent.js
@@ -0,0 +1,14 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 17-08-2017
+ * |
+ * | File Name:     src/agent.js
+ * +===============================================
+ */
+const mongorito = require('mongorito')
+
+class Agent extends mongorito.Model {}
+
+module.exports = Agent
