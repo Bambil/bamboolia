75db02553e1fcf674d92264b4493e8589a5068ae
Parham Alvani <parham.alvani@gmail.com>
2017-08-18 00:18:44 +0430

    Adds bamboo device manager.

---
+++ src/dm.js
@@ -0,0 +1,37 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 17-08-2017
+ * |
+ * | File Name:     src/dm.js
+ * +===============================================
+ */
+const Agent = require('./agent')
+const mongorito = require('mongorito')
+
+class BambooDM {
+  constructor (url) {
+    this.db = new mongorito.Database(url)
+    this.db.register(Agent)
+  }
+
+  async addAgent (tenant, name) {
+    let a = new Agent({
+      tenant,
+      name
+    })
+    await a.save()
+  }
+
+  async removeAgent (tenant, name) {
+    let a = await Agent.findOne({tenant, name})
+    a.remove()
+  }
+
+  async run () {
+    await this.db.connect()
+  }
+}
+
+module.exports = BambooDM
