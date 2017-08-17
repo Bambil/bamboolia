9ef6a8dd7c8577d23b9f67e0bbdfa5a7ffec4d3c
Parham Alvani <parham.alvani@gmail.com>
2017-08-17 18:26:15 +0430

    Adds configuration modules.

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,10 @@
+module.exports = {
+  connectivity: {
+    host: 'BAMBOO_CONNECTIVITY_HOST',
+    port: 'BAMBOO_CONNECTIVITY_PORT'
+  },
+  http: {
+    host: 'BAMBOO_HTTP_HOST',
+    port: 'BAMBOO_HTTP_PORT'
+  }
+}
---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  connectivity: {
+    host: '127.0.0.1',
+    port: 1883
+  },
+  http: {
+    host: '0.0.0.0',
+    port: 8085
+  }
+}
