c8da6a6854e5c7e7304b3538209e10a320542ef4
Parham Alvani <parham.alvani@gmail.com>
2017-08-22 12:41:30 +0430

    Bamboo type management system was added.

---
+++ src/tm.js
@@ -0,0 +1,33 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 22-08-2017
+ * |
+ * | File Name:     src/tm.js
+ * +===============================================
+ */
+const mongoose = require('mongoose')
+const Type = require('./type')
+
+class BambooTM {
+  constructor (url) {
+    mongoose.connect(url, {useMongoClient: true})
+    mongoose.Promise = global.Promise
+  }
+
+  async createModel (name, settings, states, statistics, attributes) {
+    await Type.update({ name }, {
+      name,
+      settings,
+      states,
+      statistics,
+      attributes
+    }, {
+      upsert: true,
+      runValidators: true
+    })
+  }
+}
+
+module.exports = BambooTM
