b42798564537f3e910f5842f50a52406d16ab129
Parham Alvani <parham.alvani@gmail.com>
2017-08-23 09:21:35 +0430

    Adds model route and cli command.

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const config = require('config')
 const BambooDM = require('./src/dm')
 const bambooDM = new BambooDM(config.mongo.url)
 
+/* Bamboo Device Manager */
+const BambooTM = require('./src/tm')
+const bambooTM = new BambooTM(config.mongo.url)
+
+const recursive = require('recursive-readdir')
+const YAML = require('yamljs')
+recursive('packages/standard', function (err, files) {
+  if (err) {
+    return
+  }
+  files.forEach((file) => {
+    let m = YAML.load(file)
+    bambooTM.createModel(`${m.package}.${m.name}`, m.settings, m.states, m.statistics, m.attributes)
+  })
+})
+
 /* Command Line Interface */
 const vorpal = require('vorpal')()
 const chalk = require('chalk')
@@ -28,6 +44,19 @@ vorpal
       this.log(`Name: ${chalk.rgb(102, 204, 255)(a.name)}`)
       this.log(`Time: ${chalk.rgb(255, 255, 255)(a.time)}`)
       this.log(`Things: ${chalk.rgb(255, 255, 255)(a.things)}`)
+    }
+  })
+
+vorpal
+  .command('model [name]', 'thing model')
+  .action(async function (args) {
+    let m = await bambooTM.getModel(args.name)
+    if (m) {
+      this.log(`Name: ${m.name}`)
+      this.log(`Statistics: ${m.statistics}`)
+      this.log(`States: ${m.states}`)
+      this.log(`Settings: ${m.settings}`)
+      this.log(`Attributes: ${m.attributes}`)
     }
   })
 
@@ -67,23 +96,34 @@ server.connection({
   port: config.http.port
 })
 
-server.route({
-  method: 'GET',
-  path: '/agent',
-  handler: async function (request, reply) {
-    let as = await bambooDM.all()
-    let res = {}
+server.route([
+  {
+    method: 'GET',
+    path: '/agent',
+    handler: async function (request, reply) {
+      let as = await bambooDM.all()
+      let res = {}
 
-    for (let a of as) {
-      res[`${a.tenant}/${a.name}`] = {
-        time: new Date(a.time).toISOString(),
-        things: a.things
+      for (let a of as) {
+        res[`${a.tenant}/${a.name}`] = {
+          time: new Date(a.time).toISOString(),
+          things: a.things
+        }
       }
+
+      return reply(res)
+    }
+  }, {
+    method: 'GET',
+    path: '/model/{name}',
+    handler: async function (request, reply) {
+      let m = await bambooTM.getModel(request.params.name)
+
+      return reply(m)
     }
 
-    return reply(res)
   }
-})
+])
 
 server.start((err) => {
   if (err) {
